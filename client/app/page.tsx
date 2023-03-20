0cdd6097180670e4481417d9e200e8bd3daed9bb
Thomas Vu <thomasjvu@gmail.com>
2023-03-19 21:49:05 -0400

    Update socket.io server

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -5,7 +5,7 @@ import { useDraw } from "@/hooks/useDraw";
 import { ChromePicker } from 'react-color'
 import { io } from "socket.io-client"
 import { drawLine } from "@/utils/drawLine";
-const socket = io('http://localhost:3001')
+const socket = io('https://draw-with-me-server-5ouf.onrender.com')
 
 interface pageProps {}
 
